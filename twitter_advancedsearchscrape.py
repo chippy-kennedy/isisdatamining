8182e92ff4f2b0f5f837e1aaba17b02a43660d2a
Chippy <chippy@case.edu>
2016-04-20 16:17:39 -0400

    started hookup to sql db

--- twitter_advancedsearchscrape.py
+++ twitter_advancedsearchscrape.py
@@ -1,18 +1,29 @@
 #TWITTER DATA COLLECTION
 
 import MySQLdb
+import urllib
 
-db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
+
+db = MySQLdb.connect(host="localhost",user="testuser",passwd="test123",db="TESTDB" )
 dbcursor = db.cursor()
 
 #Prep a new DB Table
-sql = "CREATE TABLE TWEET-METRICS (
-			ATTACK_ID CHAR(40) FOREIGN KEY ATTACKS,
-			DAY0_TWEETCOUNT INTEGER,
-			DAY1_TWEETCOUNT INTEGER,
-			DAY2_TWEETCOUNT INTEGER,
-			DAY3_TWEETCOUNT INTEGER)"
+baseSQL ="""CREATE TABLE TWEET-METRICS (
+			AttackID CHAR(40) FOREIGN KEY ATTACKS,
+			TweetCount INTEGER,
+			StartDate DATE,
+			EndDate DATE,
+			SearchTerms CHAR(40)"""
+dbcursor.execute(baseSQL)
 
-searchURL = "https://twitter.com/search?q=Damaturu%2C%20Nigeria&src=typd"i
 
+searchURL = "https://twitter.com/search?q=Damaturu%2C%20Nigeria&src=typd"
+response = urllib2.urlopen(searchURL)
+
+insertSQL = """INSERT INTO TWEET-METRICS (AttackID, TweetCount, StartDate, EndDate, SearchTerms)
+				VALUES (1, 100, 2015-04-12, 2015-06-14, 'terrorist')""" 
+dbcursor.execute(insertSQL)
+
+
+db.close()
 
