6707dceb7b2cd4bbbe448453a13af7f338b528c3
Chippy <chippy@case.edu>
2016-04-21 09:45:33 -0400

    pulled in GetOldTweets lin, modified Main for our use. TODO: SQL Error on query

--- twitter_advancedsearchscrape.py
+++ twitter_advancedsearchscrape.py
@@ -1,21 +1,20 @@
 #TWITTER DATA COLLECTION
 
+import urllib,urllib2,re,datetime
 import time
 import sqlite3
-import urllib
+from pyquery import PyQuery as pq
 from lxml import html
 import requests
 import TwitterRequest as tr
-import ssl
-import urllib3
-import certifi
+import json
 
 
 # Handle Web Certificate Approval
-http = urllib3.PoolManager(
-    cert_reqs='CERT_REQUIRED', # Force certificate check.
-    ca_certs=certifi.where(),  # Path to the Certifi bundle.
-)
+#http = urllib3.PoolManager(
+#    cert_reqs='CERT_REQUIRED', # Force certificate check.
+#    ca_certs=certifi.where(),  # Path to the Certifi bundle.
+#)
 
 
 BASEURL = "https://twitter.com/search?"
@@ -24,20 +23,29 @@ SEARCHTERMS = "terrorist bangladesh isil"
 #q=terrorist%20bangladesh&src=typd&lang=en"
 
 # 1. Setup Twitter Search Query
-request = tr.TwitterRequest(SEARCHTERMS)
-headers = {'User-agent' : 'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25'}
-query = request.getURL()
-res = http.get(query)
-response = requests.get(query, headers = headers)
-print query
+twitterURL = tr.TwitterRequest(SEARCHTERMS)
+url = twitterURL.getURL()
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+req = urllib2.Request(url, headers = headers)
+jsonResponse = urllib2.urlopen(req).read()
+
+dataJson = json.loads(jsonResponse)
+
+
+refreshCursor = dataJson['min_position']			
+tweets = PyQuery(dataJson['items_html'])('div.js-stream-tweet')
+
+
+
+print tweets
+
 
 # 2. Parse Search Results
 tweetCount = 0
 #bool continueSearch = True
 
 #scrape page at hand
-print response
-tree = html.fromstring(response.content)
+#tree = html.fromstring(r.text)
 
 #while(continueSearch)
 #  time.sleep(10)	
